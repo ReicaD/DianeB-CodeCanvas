c9bf9efd2041092e5e8c5175bb58cf8479a5deea
ReicaD <dbamanya447@gmail.com>
2023-10-02 14:56:58 -0400

    modified with a form

--- client/src/components/Contact/contact.js
+++ client/src/components/Contact/contact.js
@@ -1,11 +1,8 @@
 import React from "react";
 import "./contact.css";
-import Linkedin from "../../assets2/linkedin.png"
-import Github from "../../assets2/github.png"
- 
-
-
-
+import Linkedin from "../../assets2/linkedin.png";
+import Github from "../../assets2/github.png";
+// import { Link } from "react-scroll";
 
 function Contact() {
   return (
@@ -17,14 +14,34 @@ function Contact() {
           independently and with a team creating effective presentations and
           developing opportunities that further establish organizational goals.
         </p>
-        <div className="clientImg">
-
-            <img src={Linkedin} alt="Client" className="clientImg"/>
-            <img src={Github} alt="Client" className="clientImg"/>
-            
+        <div className="clientImgs">
+          <img src={Linkedin} alt="Linkedin" className="clientImg" />
+          <img src={Github} alt="Github" className="clientImg" />
         </div>
       </div>
-      <div id="contact"></div>
+      <div id="contact">
+        <h1 className="contactPageTitle"> Contact Me</h1>
+        <span className="contactDesc">
+          Please fill this below to discuss work opportunities
+        </span>
+        <form className="contactForm">
+          <input type="text" className="name" placeholder="your name" />
+          <input type="email" className="email" placeholder="your email" />
+          <textarea
+            className="msg"
+            name="message"
+            rows="5"
+            placeholder=" your message"
+          ></textarea>
+          <button type="submit" value="send" className="submitBtn">
+            submit
+          </button>
+          <div className="links">
+            <img src={"link"} alt="" className="link" />
+            <img src={"link"} alt="" className="link" />
+          </div>
+        </form>
+      </div>
     </section>
   );
 }
