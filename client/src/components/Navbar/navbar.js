48473eff25249af42afa8de76023ffcf2cc7c8dc
ReicaD <dbamanya447@gmail.com>
2023-10-24 00:57:22 -0400

    modified with links

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -9,12 +9,14 @@ function Navbar() {
     <nav className="navbar">
       <img src={logo} alt="Logo" className="logo" />
       <div className="desktopMenu">
-        <Link to="home" className="desktopMenuListItem">Home</Link>
-        <Link to="about"  className="desktopMenuListItem">About</Link>
-        <Link to="portfolio" className="desktopMenuListItem">Portfolio</Link>
-        <Link to="client" className="desktopMenuListItem">Client</Link>
+        <Link activeClass="active" to="intro"  spy={true} smooth={true} offset={100} duration={500} className="desktopMenuListItem">Home</Link>
+        <Link activeClass="active" to="skills"  spy={true} smooth={true} offset={100} duration={500} className="desktopMenuListItem">About</Link>
+        <Link activeClass="active" to="works"  spy={true} smooth={true} offset={100} duration={500} className="desktopMenuListItem">Portfolio</Link>
+        <Link activeClass="active" to="client"  spy={true} smooth={true} offset={100} duration={500} className="desktopMenuListItem">Client</Link>
       </div>
-      <button>
+      <button className="desktopMenuBtn" onClick={()=>{
+         document.getElementById("contact").scrollIntoView({behaviour:"smooth"});
+      }}>
         <img src={contactImg} alt="" className="desktopMenuImg" />
         Contact Me
       </button>
@@ -23,3 +25,7 @@ function Navbar() {
 }
 
 export default Navbar;
+
+{/* <Link to="about"  className="desktopMenuListItem">About</Link>
+<Link to="portfolio" className="desktopMenuListItem">Portfolio</Link>
+<Link to="client" className="desktopMenuListItem">Client</Link> */}
